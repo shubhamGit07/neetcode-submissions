12dabe4b0e616663f5c1b3023e36a95f0bfe1e69
Shubham Kumar Mishra <shubhamkumar6000ara@gmail.com>
2026-07-09 08:13:11 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        String str = s.replaceAll("[^a-zA-Z0-9]" , "").toLowerCase();
+
+        int i = 0;
+        int j = str.length()-1;
+
+        while(i<j){
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
+}
